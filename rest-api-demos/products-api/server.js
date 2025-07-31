19d93291ccb007245b80d25be05f38494f77dfc7
Davinder Singh <dasingh9@gmail.com>
2025-07-31 21:15:31 +1200

    Implement MVC structure, middlewares, authentication and global error handling

--- rest-api-demos/products-api/server.js
+++ rest-api-demos/products-api/server.js
@@ -1,61 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { ProductDao } from './dao/product-dao.js'
+import ProductRouter from './routes/product-route.js';
+import { errorHandler } from './middleware/error-handler.js';
+import { checkApiKey } from './middleware/auth-handler.js';
+import dotenv from 'dotenv';
+dotenv.config(); // Load env vars from .env file
+
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(bodyParser.json());
-
-const productDao = new ProductDao();
-
-app.get('/products', async (req, res) => {
-    console.log("Get all products");
-    const products = await productDao.getAllProducts();
-    console.log(JSON.stringify(products));
-    res.json(products).status(200);
-});
-
-app.post('/products', async (req, res) => {
-    const product = req.body;
-    await productDao.createProduct(product);
-    res.status(200).json(product);
-});
-
-app.put('/products/:id', async (req, res) => {
-    try {
-        const id = Number(req.params.id);
-        const product = req.body;
-        await productDao.updateProduct(id, product);
-        res.json(product).status(200);
-    }
-    catch (error) {
-        console.error(error);
-        const customError = {
-            message: "Unable to update product",
-            reason: "Unknown"
-        };
-        res.status(500).json(customError);
-    }
-});
-
-app.delete('/products/:id', async(req, res) => {
-   try {
-        const id = Number(req.params.id);
-        const product = await productDao.deleteProduct(id);
-        res.status(200).json(product);
-    }
-    catch (error) {
-        console.error(error);
-        const customError = {
-            message: "Unable to delete product",
-            reason: "Unknown"
-        };
-        res.status(500).json(customError);
-    }
-});
-
-
+app.use('/api/products', checkApiKey, ProductRouter);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
